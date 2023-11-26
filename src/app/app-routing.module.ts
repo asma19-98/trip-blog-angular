dea14e7771ae14626b484c6f6c46e6fada7de64b
helasBa <110030113+helasBa@users.noreply.github.com>
2023-11-26 16:38:12 +0100

    rangement componenet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
+import { AddBlogComponent } from './add-blog/add-blog.component';
+import { DestinationComponent } from './destination/destination.component';
+import { DestinationDetailsComponent } from './destination-details/destination-details.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent}
+  {path:'',component:AddBlogComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { AddBlogComponent } from './add-blog/add-blog.component';
+import { BlogDetailsComponent } from './blog-details/blog-details.component';
+import { DestinationComponent } from './destination/destination.component';
+import { DestinationDetailsComponent } from './destination-details/destination-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BlogComponent,
+    AddBlogComponent,
+    BlogDetailsComponent,
+    DestinationComponent,
+    DestinationDetailsComponent
   ],
   imports: [
     BrowserModule,
